75e0c7e5b12cc8bc2b5877a4c2ebb9b4d0f34e14
David Nachtigall <peacekeeper@MBP-Retina-5.local>
2015-01-25 15:35:14 +0100

    Added Table View and a TaskModel

---
+++ TaskIt/TaskModel.swift
@@ -0,0 +1,16 @@
+//
+//  TaskModel.swift
+//  TaskIt
+//
+//  Created by David Nachtigall on 25.01.15.
+//  Copyright (c) 2015 David Nachtigall. All rights reserved.
+//
+
+import Foundation
+
+
+struct TaskModel {
+    var task:String
+    var subTask: String
+    var date: String
+}
--- TaskIt/ViewController.swift
+++ TaskIt/ViewController.swift
@@ -8,11 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var taskArray:[TaskModel] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let task1 = TaskModel(task: "Study French", subTask: "Verbs", date: "01/14/2015")
+        let task2 = TaskModel(task: "Eat Dinner", subTask: "Burger", date: "01/14/2015")
+        
+        taskArray = [task1, task2, TaskModel(task: "Gym", subTask: "Legday", date: "01/14/2015")]
+        
+        
+            
+        
+        self.tableView.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +35,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.taskArray.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let thisTask = taskArray[indexPath.row]
+        
+        var cell: TaskCell = tableView.dequeueReusableCellWithIdentifier("myCell") as TaskCell
+        
+        cell.taskLabel.text = thisTask.task
+        cell.subtaskLabel.text = thisTask.subTask
+        cell.dateLabel.text = thisTask.date
+        
+        return cell
+    }
+    
+    //UITableViewDelegate
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    
 }
 
